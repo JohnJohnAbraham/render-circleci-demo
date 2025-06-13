bc6b6dd44389ac25cdd02a0fdd397e6adbf07d18
John Abraham <137336636+JohnJohnAbraham@users.noreply.github.com>
2025-06-13 18:48:01 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello from Render + CircleCI 🚀</h1>
+      <p>If you're seeing this, your zero downtime deploy worked!</p>
+    </div>
+  );
+}
+
+export default App;
